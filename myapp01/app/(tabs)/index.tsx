b65430118602c387010626cf88a14ca2e991faa3
pedrohennrique2911 <162355489+pedrohennrique2911@users.noreply.github.com>
2024-12-04 19:22:08 -0300

    Projeto 1 - React Native

---
+++ myapp01/app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Text, View} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+      }}>
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+};
+
+export default YourApp;
